d5d2055f127dfe1c27be15a7c68ebcb31ad8b39e
Nick Fireman <nfireman@oakland.edu>
2020-04-14 18:20:29 -0400

    add cross-validation experiment

--- Experiments.java
+++ Experiments.java
@@ -7,19 +7,21 @@ import weka.classifiers.Classifier;
 
 import java.io.File;
 import java.util.Enumeration;
+import java.util.Random;
 
 class Experiments {
-  static final String DatasetPath = "./combined.arff";
+  static final String datasetPath = "./combined.arff";
 
   public static void main(String[] args) throws Exception {
     SplitDataset data = splitDatasetOnProjectName("Berek");
     J48 classifier = new J48();
 
-    runExperiment(classifier, data);
+    // runExperiment(classifier, data);
+    // runCrossValidationExperiment(classifier);
   }
 
   public static SplitDataset splitDatasetOnProjectName(String projectName) {
-    Dataset dataset = Dataset.fromFile(DatasetPath);
+    Dataset dataset = new Dataset(datasetPath);
     Instances training = dataset.emptyInstances();
     Instances validation = dataset.emptyInstances();
 
@@ -35,6 +37,13 @@ class Experiments {
     return new SplitDataset(training, validation);
   }
 
+  // evaluates the given classifier using 10 times 10-fold cross-validation
+  public static void runCrossValidationExperiment(Classifier classifier, Instances data) throws Exception {
+    Evaluation eval = new Evaluation(data);
+    eval.crossValidateModel(classifier, data, 10, new Random(1));
+  }
+
+  // evaluates the given classifier using the given training and evaluation datasets
   public static void runExperiment(Classifier classifier, SplitDataset data) throws Exception {
     classifier.buildClassifier(data.training);
     Evaluation eval = new Evaluation(data.training);
@@ -44,12 +53,19 @@ class Experiments {
 
   // responsible for loading and fetching instances from our ARFF dataset
   static class Dataset {
-    public static ArffLoader loader;
-    public static Instances structure;
+    public ArffLoader loader;
+    public Instances structure;
 
-    public Dataset(ArffLoader loader, Instances structure) {
-      this.loader = loader;
-      this.structure = structure;
+    public Dataset(String path) {
+      try {
+        ArffLoader loader = new ArffLoader();
+        loader.setFile(new File(path));
+        Instances structure = loader.getStructure();
+        structure.setClassIndex(structure.numAttributes() - 1);
+
+        this.loader = loader;
+        this.structure = structure;
+      } catch (Exception e) {}
     }
 
     public Instance getNextInstance() {
@@ -60,20 +76,7 @@ class Experiments {
       }
     }
 
-    public static Dataset fromFile(String path) {
-      try {
-        ArffLoader loader = new ArffLoader();
-        loader.setFile(new File(path));
-        Instances structure = loader.getStructure();
-        structure.setClassIndex(structure.numAttributes() - 1);
-
-        return new Dataset(loader, structure);
-      } catch (Exception e) {
-        return null;
-      }
-    }
-
-    public static Instances emptyInstances() {
+    public Instances emptyInstances() {
       return new Instances(structure);
     }
   }
